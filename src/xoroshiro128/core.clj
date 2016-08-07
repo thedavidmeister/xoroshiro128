fb52c86e0bf79b631c356590fa00fe029c6c7de0
David Meister <thedavidmeister@gmail.com>
2016-08-07 22:38:12 +1000

    splitmix64 wip

--- src/xoroshiro128/core.clj
+++ src/xoroshiro128/core.clj
@@ -8,11 +8,30 @@
   (result [_])
   (next [_]))
 
-(deftype PRNG [^long a ^long b]
+(deftype Splitmix64 [^long x]
+  IPRNG
+  (result
+    [_]
+    (let [z     (+ x (unchecked-long 0x9E3779B97F4A7C15))
+          z'    (*  (bit-xor z (unsigned-bit-shift-right z 30))
+                    (unchecked-long 0xBF58476D1CE4E5B9))
+          z''   (*  (bit-xor z (unsigned-bit-shift-right z 27))
+                    (unchecked-long 0x94D049BB133111EB))
+          z'''  (bit-xor z'' (unsigned-bit-shift-right z'' 31))]
+      z'''))
+
+  (next
+    [_]
+    (Splitmix64. (+ x (unchecked-long 0x9E3779B97F4A7C15)))))
+
+(defn splitmix64 [x] (Splitmix64. x))
+
+(deftype Xoroshiro128 [^long a ^long b]
   IPRNG
   (result
     [_]
     (+ a b))
+
   (next
     [_]
     (let [x   (bit-xor a b)
@@ -20,15 +39,15 @@
                         x
                         (bit-shift-left x 14))
           b'  (Long/rotateLeft x 36)]
-      (PRNG. a' b'))))
+      (Xoroshiro128. a' b'))))
 
-(defn prng [a b] (PRNG. a b))
+(defn xoroshiro128 [a b] (Xoroshiro128. a b))
 
 (defn generator
   [a b]
-  (map result (iterate next (PRNG. a b))))
+  (map result (iterate next (Xoroshiro128. a b))))
 
-(def simple-state (atom (PRNG. 1 3)))
+(def simple-state (atom (Xoroshiro128. 1 2)))
 
 (defn rand
   []
--- test/xoroshiro128/core_test.clj
+++ test/xoroshiro128/core_test.clj
@@ -5,5 +5,7 @@
 
 (deftest a
   []
-  (time (prn "native" (.nextLong (java.util.Random.))))
-  (time (prn "xoro" (xoroshiro128.core/rand))))
+  ; (time (prn "native" (.nextLong (java.util.Random.))))
+  ; (time (prn "xoro" (xoroshiro128.core/rand)))
+  ; (time (prn "xoro" (xoroshiro128.core/rand)))
+  (xoroshiro128.core/next (xoroshiro128.core/splitmix64 1)))
